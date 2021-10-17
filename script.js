ea78d49fa087063e72df55c63f508626633f247a
unknown <ijlnflhbrzdev@gmail.com>
2021-10-17 10:53:12 +0700

    problem solved

--- script.js
+++ script.js
@@ -14,28 +14,23 @@ function resetFilter() {
 
           if (listFormControll[i].getAttribute("type") == "checkbox") {
                listFormControll[i].checked = false;
-               console.log("True");
           } else {
                listFormControll[i].value = "";
-               console.log("False");
           }
-
-          // console.log("Hello Wordl")
      }
 }
 
 function showFilterProduk() {
-     document.querySelector(".filter-produk").classList.toggle("show-filter")
+     document.querySelector(".filter-produk").classList.add("show-filter");
      let ui_show_filter = document.querySelector('.filter-produk');
      ui_show_filter.classList.add('ui_show_filter_fixed');
-
      document.querySelector(".btn-show-filter").classList.add("d-none")
 }
 
 function closeFilterProduk() {
-     let ui_show_filter = document.querySelector('.ui_show_filter_fixed');
-     ui_show_filter.classList.add('close_show_produk');
-
+     document.querySelector(".filter-produk").classList.remove("show-filter");
+     let ui_show_filter = document.querySelector('.filter-produk');
+     ui_show_filter.classList.remove('ui_show_filter_fixed');
      document.querySelector(".btn-show-filter").classList.remove("d-none")
 }
 
